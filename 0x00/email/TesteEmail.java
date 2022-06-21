bf157c0a97cb42a287eb3b1a9d185ec455f79a8f
lorenzas <lorenza@ciandt.com>
2022-06-21 13:52:00 -0300

    Adicionando task 0

--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -3,9 +3,9 @@ public class TesteEmail {
         return Pessoa.emailValid("email_teste@dominio.com.br");
     }
     public static boolean testar_email_sem_arroba() {
-        return Pessoa.emailValid("email_testedominio.com.br");
+        return !Pessoa.emailValid("email_testedominio.com.br");
     }
     public static boolean testar_email_mais_50_caracteres() {
-        return Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
+        return !Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
     }
 }
