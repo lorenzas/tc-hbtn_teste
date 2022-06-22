fe81e09c450ab28f928bf25617c9b6d71ff96231
lorenzas <lorenza@ciandt.com>
2022-06-22 09:00:14 -0300

    Adicionando task 0

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -7,9 +7,11 @@ public class TesteEmail {
     public static void testar_email_com_arroba() {
         assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
     }
+    @Test
     public static void testar_email_sem_arroba() {
         assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
     }
+    @Test
     public static void testar_email_mais_50_caracteres() {
         assertEquals(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"), false);
     }
