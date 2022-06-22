1013277647134f79349a277e4442fb090a6cbad8
lorenzas <lorenza@ciandt.com>
2022-06-22 08:56:54 -0300

    Adicionando task 0

---
+++ 0x01/email/src/TesteEmail.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TesteEmail {
+    @Test
+    public static void testar_email_com_arroba() {
+        assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
+    }
+    public static void testar_email_sem_arroba() {
+        assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
+    }
+    public static void testar_email_mais_50_caracteres() {
+        assertEquals(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"), false);
+    }
+}
